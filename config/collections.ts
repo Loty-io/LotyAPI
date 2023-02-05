4effd069ee2339d368d978dc62a0d034a3e39257
Ricardo Hernández <rmoguel@MacBook-Pro.local>
2023-02-04 16:51:38 -0800

    add loty main test collection

--- config/collections.ts
+++ config/collections.ts
@@ -22,12 +22,19 @@ const lotyTest: Collection = {
   contractType: ContractType.NFTCollection,
 };
 
+const lotyDropTest: Collection = {
+  contractAddress: "0x014B285560caFE34E249ea15aC86d051930C54Ac",
+  contractType: ContractType.NFTDrop,
+};
+
 const collectionsStaging: Collections = {
   "0x6ae3316f2193D45780C3BA6fdc88Fdbd182E0667": spaInTheHouse,
   "0x7c435572A6BD83465d918310086Ca0da186CF804": lotyTest,
 };
 
-const collectionProd: Collections = {};
+const collectionProd: Collections = {
+  "0x014B285560caFE34E249ea15aC86d051930C54Ac": lotyDropTest,
+};
 
 export type { Collection, Collections };
 export { collectionsStaging, collectionProd, ContractType };
